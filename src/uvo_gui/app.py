20481d4f831bd5380c48fc86b2e7c3536893462d
Michal Maxian <michal@maxian.sk>
2026-04-04 10:48:35 +0200

    feat: wire up all pages in app.py, remove old nav_header and detail_dialog

--- src/uvo_gui/app.py
+++ src/uvo_gui/app.py
@@ -3,7 +3,10 @@
 from nicegui import ui
 
 # Import pages to register @ui.page decorators
+import uvo_gui.pages.about  # noqa: F401
+import uvo_gui.pages.procurers  # noqa: F401
 import uvo_gui.pages.search  # noqa: F401
+import uvo_gui.pages.suppliers  # noqa: F401
 from uvo_gui.config import GuiSettings
 
 settings = GuiSettings()
